f7d4ca5bf76ca0dddffb5891858de239d9b73968
Vinicius Gama Santos <114769520+viniciusggama@users.noreply.github.com>
2023-06-18 09:10:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gerenciador_tarefas
+ REACT
---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import "./App.css"
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text :"Desenvolver aplicação REACT",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text :"Surfar",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      text :"Treinar",
+      category: "pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 5,
+      text :"Estudar react ate  infinito",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ])
+
+  return (
+  <div className='app' > 
+      <h1>Tarefas da Semana</h1>
+      <div className='todo-list'>
+        {todos.map((todo) => (
+          <div className='todo'>
+            <div className='content'>
+              <p>{todo.text}</p>
+              <p className='category'>({todo.category})</p>
+            </div>
+            <div>
+              <button>COMPLETAR</button>
+              <button>X</button>
+            </div>
+          </div>
+        ))}
+      </div>
+  </div>
+  )
+}
+
+export default App
